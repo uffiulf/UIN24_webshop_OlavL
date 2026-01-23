2501a944ab0b3fa9d6f9cc8d6ad4db03dcecc22a
uffiulf <sofnbeat@gmail.com>
2026-01-23 12:20:33 +0100

    hadde problemer med git, fikset

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Title from './components/Title'
+
+function App() {
+
+  return (
+    <main>
+        <Title/>
+       <h2>Dette er kjempegøy</h2>
+   </main>
+  )
+}
+
+export default App
---
+++ src/components/Title.jsx
@@ -0,0 +1,3 @@
+export default function Title(){
+    return<h1>Mitt første react prosjekt</h1>
+}
